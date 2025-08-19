cfd8c293e48b915a7301fcc693214313071fdc45
Ali Issa <alyy.issa01@gmail.com>
2025-08-19 19:14:57 +0300

    component: Navbar

---
+++ client/src/assets/assets.js
@@ -0,0 +1,15 @@
+import logo from "./navbar/logo.svg"
+import close_icon from "./navbar/close_icon.svg"
+import menu_icon from "./navbar/menu_icon.svg"
+
+export const assets = {
+    logo,
+    close_icon,
+    menu_icon
+}
+
+export const menuLinks = [
+    {name: "Home", path: "/"},
+    {name: "Projects", path: "/projects"},
+    {name: "Courses", path: "/courses"}
+]
